004cf22fd6edf6f5b2b391be2da011dd8b6e5c10
coelzs <coelzs1116@gmail.com>
2024-08-03 02:38:11 +0900

    reset

--- Back-End/src/main/java/com/dongne/its/base/dao/ProjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dongne.its.base.dao;
-
-import com.dongne.its.base.domain.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-}
--- Back-End/src/main/java/com/dongne/its/base/domain/Role.java
+++
@@ -1,5 +0,0 @@
-package com.dongne.its.base.domain;
-
-public enum Role {
-    ADMIN, PL, DEV, TESTER
-}
