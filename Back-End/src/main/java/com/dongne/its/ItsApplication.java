1c9a5ececcc0c00e0db6959b0518de631cbb167a
Hossmot <83452955+Hossmot@users.noreply.github.com>
2024-08-12 02:15:15 +0900

    Fix : Fix conflict from main branch

--- Back-End/src/main/java/com/dongne/its/ItsApplication.java
+++ Back-End/src/main/java/com/dongne/its/ItsApplication.java
@@ -2,6 +2,9 @@ package com.dongne.its;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ItsApplication {
@@ -9,4 +12,5 @@ public class ItsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ItsApplication.class, args);
 	}
+
 }
