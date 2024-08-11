3e23d8538e3ffbdfb368faf8af8b5e291c53558e
Hossmot <83452955+Hossmot@users.noreply.github.com>
2024-08-12 02:23:47 +0900

    Fix : Fix conflict from main branch

--- Back-End/src/main/java/com/dongne/its/config/OpenApiConfig.java
+++ Back-End/src/main/java/com/dongne/its/config/OpenApiConfig.java
@@ -8,12 +8,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class OpenApiConfig {
 
-  @Bean
-  public OpenAPI customOpenAPI() {
-    return new OpenAPI()
-        .info(new Info()
-            .title("API Documentation")
-            .version("1.0")
-            .description("API documentation for the application"));
-  }
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API Documentation")
+                        .version("1.0")
+                        .description("API documentation for the application"));
+    }
 }
