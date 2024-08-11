b1e1302b705e8fd20a408127697c2a9cd0e42c1f
IDLE <83659656+idle2534@users.noreply.github.com>
2024-08-12 01:56:00 +0900

    Feat : Add BaseEntity

---
+++ Back-End/src/main/java/com/dongne/its/base/domain/BaseEntity.java
@@ -0,0 +1,31 @@
+package com.dongne.its.base.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+@Data
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Column(name = "created_at", updatable = false)
+    @CreatedDate
+    private Instant createdAt;
+
+    @Column(name = "updated_at")
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deletedAt;
+
+    @LastModifiedBy
+    private String lastModifiedBy;
+}
