b21485e3a56f0e7dde75c1ad546232ff737a9fb5
IDLE <83659656+idle2534@users.noreply.github.com>
2024-08-15 17:30:21 +0900

    Fix : Fix query of repository and membercontroller follow apiresponse

--- Back-End/src/main/java/com/dongne/its/member/web/controller/MemberController.java
+++ Back-End/src/main/java/com/dongne/its/member/web/controller/MemberController.java
@@ -1,7 +1,9 @@
 package com.dongne.its.member.web.controller;
 
+import com.dongne.its.base.apiPayload.code.ApiResponse;
 import com.dongne.its.member.converter.MemberConverter;
 import com.dongne.its.member.domain.Member;
+import com.dongne.its.member.domain.enums.Role;
 import com.dongne.its.member.service.MemberCommandService;
 import com.dongne.its.member.service.MemberQueryService;
 import com.dongne.its.member.web.dto.*;
@@ -11,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/member")
 @RequiredArgsConstructor
@@ -20,62 +21,76 @@ public class MemberController {
     private final MemberQueryService memberQueryService;
     private final MemberCommandService memberCommandService;
 
+
     //Put
     @PutMapping("/account/update")
-    public ResponseEntity<MemberResponseDto> update(@RequestBody MemberRoleUpdateRequestDto request) {
+    public ResponseEntity<MemberResponseDto> update(@RequestHeader("id") Long id, @RequestBody MemberRoleUpdateRequestDto request) {
+        Member member_request = memberQueryService.findMemberById(id);
+        memberCommandService.checkRole(member_request, Role.ADMIN);
         Member member_target = memberCommandService.updateRole(request);
-        return ResponseEntity.ok(MemberConverter.toMemberResponseDto(member_target));
+        return ApiResponse.onSuccess(MemberConverter.toMemberResponseDto(member_target));
     }
 
     @PutMapping("/account/delete")
-    public ResponseEntity<MemberResponseDto> delete(@RequestBody MemberDeleteRequestDto request) {
+    public ResponseEntity<MemberResponseDto> delete(@RequestHeader("id") Long id, @RequestBody MemberDeleteRequestDto request) {
+        Member member_request = memberQueryService.findMemberById(id);
+        memberCommandService.checkRole(member_request, Role.ADMIN);
         Member member_target = memberCommandService.delete(request);
-        return ResponseEntity.ok(MemberConverter.toMemberResponseDto(member_target));
+        return ApiResponse.onSuccess(MemberConverter.toMemberResponseDto(member_target));
     }
+
 
     //Post
     @PostMapping("/signUp")
-    public ResponseEntity<MemberResponseDto> signUp(@RequestBody MemberSignUpRequestDto request) {
+    public ResponseEntity<MemberResponseDto> signUp(@RequestHeader("id") Long id, @RequestBody MemberSignUpRequestDto request) {
+        Member member_request = memberQueryService.findMemberById(id);
+        memberCommandService.checkRole(member_request, Role.ADMIN);
         Member member_target = memberCommandService.signUp(request);
-        return ResponseEntity.ok(MemberConverter.toMemberResponseDto(member_target));
+        return ApiResponse.onSuccess(MemberConverter.toMemberResponseDto(member_target));
     }
 
     @PostMapping("/signIn")
     public ResponseEntity<MemberResponseDto> signIn(@RequestBody MemberSignInRequestDto request) {
         Member member = memberCommandService.signIn(request);
         System.out.println(MemberConverter.toMemberResponseDto(member).getId() + "/" + MemberConverter.toMemberResponseDto(member).getSignId() + "/" + MemberConverter.toMemberResponseDto(member).getRole());
-        return ResponseEntity.ok(MemberConverter.toMemberResponseDto(member));
+        return ApiResponse.onSuccess(MemberConverter.toMemberResponseDto(member));
     }
 
     @PostMapping("/createAdmin")
     public ResponseEntity<MemberResponseDto> createAdmin(@RequestBody MemberSignUpRequestDto request) {
         Member member = memberCommandService.createAdmin(request);
-        return ResponseEntity.ok(MemberConverter.toMemberResponseDto(member));
+        return ApiResponse.onSuccess(MemberConverter.toMemberResponseDto(member));
     }
 
 
     //Get
     @GetMapping("/find")
-    public ResponseEntity<MemberResponseDto> find(@RequestParam Long id) {
+    public ResponseEntity<MemberResponseDto> find(@RequestHeader("id") Long id) {
         Member member = memberQueryService.findMemberById(id);
-        return ResponseEntity.ok(MemberConverter.toMemberResponseDto(member));
+        return ApiResponse.onSuccess(MemberConverter.toMemberResponseDto(member));
     }
 
     @GetMapping("/account")
-    public ResponseEntity<List<MemberResponseDto>> account() {
+    public ResponseEntity<List<MemberResponseDto>> account(@RequestHeader("id") Long id) {
+        Member member_request = memberQueryService.findMemberById(id);
+        memberCommandService.checkRole(member_request, Role.ADMIN);
         List<Member> member = memberQueryService.findMembers();
-        return ResponseEntity.ok(MemberConverter.toListMemberResponseDto(member));
+        return ApiResponse.onSuccess(MemberConverter.toListMemberResponseDto(member));
     }
 
     @GetMapping("/account/project")
-    public ResponseEntity<List<MemberResponseDto>> project(@RequestParam Long id, @RequestParam Long projectId) {
-        List<Member> member = memberQueryService.findMembersByIdAndProjectId(id, projectId);
-        return ResponseEntity.ok(MemberConverter.toListMemberResponseDto(member));
+    public ResponseEntity<List<MemberResponseDto>> project(@RequestHeader("id") Long id, @RequestParam("projectId") Long projectId) {
+        Member member_request = memberQueryService.findMemberById(id);
+        memberCommandService.checkRole(member_request, Role.ADMIN);
+        List<Member> member = memberQueryService.findMembersByProjectId(projectId);
+        return ApiResponse.onSuccess(MemberConverter.toListMemberResponseDto(member));
     }
 
     @GetMapping("/account/project/role")
-    public ResponseEntity<List<MemberResponseDto>> role(@RequestParam Long id, @RequestParam Long projectId, @RequestParam String role) {
-        List<Member> member = memberQueryService.findMembersByIdAndProjectIdAndRole(id, projectId, role);
-        return ResponseEntity.ok(MemberConverter.toListMemberResponseDto(member));
+    public ResponseEntity<List<MemberResponseDto>> role(@RequestHeader("id") Long id, @RequestParam("projectId") Long projectId, @RequestParam("role") String role) {
+        Member member_request = memberQueryService.findMemberById(id);
+        memberCommandService.checkRole(member_request, Role.ADMIN);
+        List<Member> member = memberQueryService.findMembersByProjectIdAndRole(projectId, role);
+        return ApiResponse.onSuccess(MemberConverter.toListMemberResponseDto(member));
     }
 }
