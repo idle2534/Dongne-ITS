b7074d88b84ab870b45bd98a1fc034a811cd9ada
IDLE <83659656+idle2534@users.noreply.github.com>
2024-08-23 00:17:02 +0900

    Fix : Fix Query about isDeleted and enum

--- Back-End/src/main/java/com/dongne/its/member/repository/MemberRepository.java
+++ Back-End/src/main/java/com/dongne/its/member/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package com.dongne.its.member.repository;
 
 import com.dongne.its.member.domain.Member;
+import com.dongne.its.member.domain.enums.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,7 +13,7 @@ import java.util.Optional;
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    @Query("select m from Member m where m.id = :id and m.isDeleted = false")
+    @Query("select m from Member m where m.id = :id")
     public Optional<Member> findMemberById(@Param("id") Long id);
 
     @Query("select m from Member m where m.signId = :signId and m.isDeleted = false")
@@ -21,9 +22,9 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @Query("select m from Member m where m.role != 'ADMIN' and m.isDeleted = false")
     public List<Member> findAllMembers();
 
-    @Query("SELECT pm.member FROM ProjectMembers pm WHERE pm.project.id = :projectId")
-    public List<Member> findByProjectId(@Param("projectId") Long projectId);
+    @Query("SELECT pm.member FROM ProjectMember pm WHERE pm.project.id = :projectId")
+    public List<Member> findMembersByProjectId(@Param("projectId") Long projectId);
 
-    @Query("SELECT pm.member FROM ProjectMembers pm WHERE pm.project.id = :projectId AND pm.member.role = :role")
-    public List<Member> findByProjectIdAndRole(@Param("projectId") Long projectId, @Param("role") String role);
+    @Query("SELECT pm.member FROM ProjectMember pm WHERE pm.project.id = :projectId AND pm.member.role = :role")
+    public List<Member> findMembersByProjectIdAndRole(@Param("projectId") Long projectId, @Param("role") Role role);
 }
